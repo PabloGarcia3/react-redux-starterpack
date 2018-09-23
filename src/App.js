976a722a31f98c47d28b8fe68f42fa426b229ad9
Pablo Garcia <pg3development@gmail.com>
2018-09-23 13:44:18 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import logo from "./logo.svg";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 class App extends Component {
